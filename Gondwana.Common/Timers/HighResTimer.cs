8b619652fa5f6864affce6577c16bdf36cd5d9be
Mike Adkins <159068551+mike-adkins@users.noreply.github.com>
2025-06-18 11:21:43 -0400

    updating HighResTimer

---
+++ Gondwana.Common/Timers/HighResTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace Gondwana;
+
+public static class HighResTimer
+{
+    /// <summary>
+    /// The number of ticks per second for the system's high-resolution timer.
+    /// </summary>
+    public static long TicksPerSecond => Stopwatch.Frequency;
+
+    /// <summary>
+    /// Indicates whether a high-resolution performance counter is available.
+    /// </summary>
+    public static bool HighPerfSupported => Stopwatch.IsHighResolution;
+
+    /// <summary>
+    /// Gets the current tick count using the high-resolution timer.
+    /// </summary>
+    public static long GetCurrentTickCount()
+    {
+        return Stopwatch.GetTimestamp();
+    }
+
+    /// <summary>
+    /// Returns the elapsed time in seconds between two tick counts.
+    /// </summary>
+    public static double GetDuration(long start, long stop)
+    {
+        return (double)(stop - start) / TicksPerSecond;
+    }
+
+    /// <summary>
+    /// Returns the elapsed time in seconds since the given start tick.
+    /// </summary>
+    public static double GetElapsedSince(long start)
+    {
+        return GetDuration(start, GetCurrentTickCount());
+    }
+}
--- Gondwana.Common/Timers/Timer.cs
+++ Gondwana.Common/Timers/Timer.cs
@@ -1,58 +1,56 @@
 using Gondwana.Common.Enums;
 using Gondwana.Common.EventArgs;
-using System;
 
-namespace Gondwana.Common.Timers
+namespace Gondwana.Common.Timers;
+
+public class Timer : IDisposable
 {
-    public class Timer : IDisposable
+    public event TimerEventHandler Tick;
+
+    public TimerType Type;
+    public TimerCycles Cycles;
+    public long Length;
+
+    internal long StartTick;
+    internal long LastEventTick;
+    internal bool engineTimer;
+
+    internal Timer(TimerType timerType, TimerCycles timerCycles, long startTick, double len)
     {
-        public event TimerEventHandler Tick;
-
-        public TimerType Type;
-        public TimerCycles Cycles;
-        public long Length;
-
-        internal long StartTick;
-        internal long LastEventTick;
-        internal bool engineTimer;
-
-        internal Timer(TimerType timerType, TimerCycles timerCycles, long startTick, double len)
-        {
-            Type = timerType;
-            Cycles = timerCycles;
-            StartTick = startTick;
-            LastEventTick = startTick;
-            Length = (long)(len * (double)HighResTimer.TicksPerSecond);
-            Paused = false;
-        }
-
-        ~Timer()
-        {
-            this.Dispose();
-        }
-
-        public string TimerID
-        {
-            get;
-            internal set;
-        }
-
-        internal void RaiseTick()
-        {
-            // raise the event if anyone's listening
-            if (Tick != null)
-                Tick(new TimerEventArgs(this));
-        }
-
-        public bool Paused { get; set; }
-
-        #region IDisposable Members
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-            Timers._timers.Remove(TimerID);
-            this.Tick = null;
-        }
-        #endregion
+        Type = timerType;
+        Cycles = timerCycles;
+        StartTick = startTick;
+        LastEventTick = startTick;
+        Length = (long)(len * (double)HighResTimer.TicksPerSecond);
+        Paused = false;
     }
+
+    ~Timer()
+    {
+        this.Dispose();
+    }
+
+    public string TimerID
+    {
+        get;
+        internal set;
+    }
+
+    internal void RaiseTick()
+    {
+        // raise the event if anyone's listening
+        if (Tick != null)
+            Tick(new TimerEventArgs(this));
+    }
+
+    public bool Paused { get; set; }
+
+    #region IDisposable Members
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        Timers._timers.Remove(TimerID);
+        this.Tick = null;
+    }
+    #endregion
 }
