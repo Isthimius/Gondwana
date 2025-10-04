a15177d86441c98794ff3bba13b537175b840560
Mike Adkins <mikeleeisback@gmail.com>
2025-10-03 20:12:51 -0400

    new Gondwana.ParticleTest

---
+++ Gondwana.ParticleTest/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Gondwana.ParticleTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            winFormBitmapRenderSurfaceControl1 = new Gondwana.WinForms.Rendering.WinFormBitmapRenderSurfaceControl();
+            SuspendLayout();
+            // 
+            // winFormBitmapRenderSurfaceControl1
+            // 
+            winFormBitmapRenderSurfaceControl1.Location = new Point(48, 24);
+            winFormBitmapRenderSurfaceControl1.Name = "winFormBitmapRenderSurfaceControl1";
+            winFormBitmapRenderSurfaceControl1.Size = new Size(740, 395);
+            winFormBitmapRenderSurfaceControl1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(winFormBitmapRenderSurfaceControl1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private WinForms.Rendering.WinFormBitmapRenderSurfaceControl winFormBitmapRenderSurfaceControl1;
+    }
+}
---
+++ Gondwana.ParticleTest/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gondwana.ParticleTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
