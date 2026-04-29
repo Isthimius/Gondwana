cb5ae13eccba73299cbc9a3f76af807ed62ec89c
Mike Adkins <159068551+mike-adkins@users.noreply.github.com>
2026-04-29 10:00:34 -0400

    commenting out trace write

--- Demos/SpotGL/GameWindow.cs
+++ Demos/SpotGL/GameWindow.cs
@@ -34,10 +34,10 @@ internal partial class GameWindow : Form
     {
         base.OnLoad(e);
         _gameHost = new SpotGLGameHost(renderSurface);
-        _gameHost.Engine.CPSCalculated += (cps) =>
-        {
-            Engine.Logger.LogTrace(cps.ToString());
-        };
+        //_gameHost.Engine.CPSCalculated += (cps) =>
+        //{
+        //    Engine.Logger.LogTrace(cps.ToString());
+        //};
 
         // Subscribe before Initialize() is called so the handler fires during initialization.
         _gameHost.Engine.InitializationComplete += () =>
@@ -57,7 +57,7 @@ internal partial class GameWindow : Form
         // resize client area to include the menu strip
         this.ClientSize = new Size(DefaultWindowSize.Width, DefaultWindowSize.Height + _menuStrip.Height);
 
-        _gameHost!.Initialize(logLevel: LogLevel.Trace);    // this calls Engine.Initialize + Start(SynchronizationContext.Current!)
+        _gameHost!.Initialize(logLevel: LogLevel.Warning);    // this calls Engine.Initialize + Start(SynchronizationContext.Current!)
     }
 
     protected override void OnFormClosed(FormClosedEventArgs e)
