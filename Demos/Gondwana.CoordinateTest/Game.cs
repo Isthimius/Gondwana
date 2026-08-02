d0b0f9a760283daec304171b43b24571b382e065
Mike Adkins <mikeleeisback@gmail.com>
2026-08-02 09:17:12 -0400

    fix for hex partial coordinate movement bug

--- Demos/Gondwana.CoordinateTest/Game.cs
+++ Demos/Gondwana.CoordinateTest/Game.cs
@@ -244,8 +244,8 @@ public class Game : IDisposable
     private Scene? CreateInitialScene()
     {
         var scene = new Scene();
-        var sceneLayer1 = scene.AddLayer(60, 5, 64, 64, 10, 1f, CoordinateSystemTypes.Orthogonal);
-        var sceneLayer2 = scene.AddLayer(60, 5, 32, 32, 5, 0.5f, CoordinateSystemTypes.Orthogonal);
+        var sceneLayer1 = scene.AddLayer(60, 5, 64, 64, 10, 1f, CoordinateSystemTypes.HexAxialPointedTop);
+        var sceneLayer2 = scene.AddLayer(60, 5, 32, 32, 5, 0.5f, CoordinateSystemTypes.HexAxialPointedTop);
 
         sceneLayer1.ShowGridLines = true;
         sceneLayer1.ShowCollisionBoxes = true;
