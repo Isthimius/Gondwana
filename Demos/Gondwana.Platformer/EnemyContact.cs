2a37cd13a360f4a4be841c362f7b9d28a67b7a3a
Isthimius <mike@adkinshouse.net>
2026-09-24 20:11:40 -0400

    chore: preserve usings required by linked test sources

--- Demos/Gondwana.Platformer/EnemyContact.cs
+++ Demos/Gondwana.Platformer/EnemyContact.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 
 namespace Gondwana.Demos.Platformer;
 
--- Tooling/Gondwana.Tooling.Tilesheets.WinForms/Editing/TilesheetDocument.cs
+++ Tooling/Gondwana.Tooling.Tilesheets.WinForms/Editing/TilesheetDocument.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using Gondwana.Drawing.Tilesheets.GTS;
 
 namespace Gondwana.Tooling.Tilesheets.Editing;
